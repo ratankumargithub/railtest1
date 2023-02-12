085e81e17a5dc239f8bcb96a1b2b4a88b97f954c
Ratan <ratankapr22@gmail.com>
2023-02-12 23:05:12 +0530

    added

--- src/main/java/com/masai/MyController.java
+++ src/main/java/com/masai/MyController.java
@@ -1,5 +1,21 @@
 package com.masai;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
 
+	
+	@GetMapping({"/", "/home"})
+	public String getHome() {
+		return "Home screen Get Request";
+	}
+	
+	@PostMapping("/post/data")
+	public String postHome() {
+		return "Post Successfull";
+	}
+	
 }
